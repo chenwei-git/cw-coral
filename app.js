ea76e529183696ee8a7b079f7d6627cae5cf97db
chenwei <978544851@qq.com>
2018-09-29 17:38:41 +0800

    注释

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-//app.js
+// app.js
+// App() 函数用来注册一个小程序
+// 接受一个 Object 参数，其指定小程序的生命周期回调等
 App({
-  onLaunch: function () {
+  // 生命周期回调—监听小程序初始化
+  onLaunch: function(options) {
+    // 打开小程序的路径
+    console.info("app.onlaunch: options.path =", options.path);
+    // 打开小程序的query
+    console.info("app.onlaunch: options.query =", options.query);
+    // 打开小程序的场景值
+    console.info("app.onlaunch: options.scene =", options.scene);
+    console.info("app.onlaunch: options.shareTicket =", options.shareTicket);
+    // 当场景为由从另一个小程序或公众号或App打开时，返回此字段
+    console.info("app.onlaunch: options.referrerInfo =", options.referrerInfo);
+
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -33,6 +46,33 @@ App({
       }
     })
   },
+  // 生命周期回调—监听小程序显示
+  onShow: function(options) { // options 与 onLaunch的options 一致
+    console.info("app.onShow: options =", options);
+  },
+  // 生命周期回调—监听小程序隐藏
+  onHide: function() {
+    console.info("app.onHide");
+  },
+  // 错误监听函数
+  onError: function(msg) {
+    // 错误信息，包含堆栈
+    console.error(msg);
+  },
+  // 页面不存在监听函数
+  onPageNotFound: function(res) {
+    // 不存在页面的路径
+    console.info("app.onShow: res.path =", res.path);
+    // 打开不存在页面的 query
+    console.info("app.onShow: res.query =", res.query);
+    // 是否本次启动的首个页面
+    console.info("app.onShow: res.isEntryPage =", res.isEntryPage);
+    // 如果是 tabbar 页面，请使用 wx.switchTab
+    wx.redirectTo({
+      url: 'pages/index/index'
+    });
+  },
+  // 开发者可以添加任意的函数或数据到 Object 参数中，用 this 可以访问
   globalData: {
     userInfo: null
   }
