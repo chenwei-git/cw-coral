afc250447d901bc9ecd47ca9dc3a1097039bea73
chenwei <978544851@qq.com>
2018-10-16 19:16:48 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,32 +16,6 @@ Page({
   // 页面加载时触发, 一个页面只会调用一次
   onLoad: function (query) { // 打开当前页面路径的参数
     console.log("index.onLoad: query =", query);
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      });
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        });
-      };
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
   },
   // 生命周期回调—监听页面显示
   // 页面显示/切入前台时触发
